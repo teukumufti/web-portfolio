e5cd110a6d087d8a8ed0df672ae43460c2a483e6
teukumufti <teukumufti9770@gmail.com>
2022-12-11 08:49:49 +0700

    add data dummy

---
+++ src/dataDummy/fakeData.js
@@ -0,0 +1,21 @@
+export const data = [
+    {
+        id: 1,
+        title: 'SapientNitro',
+    },
+    {
+        id: 2,
+    },
+    {
+        id: 3,
+    },
+    {
+        id: 4,
+    },
+    {
+        id: 5,
+    },
+    {
+        id: 6,
+    },
+]
