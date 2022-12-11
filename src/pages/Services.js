6c50e37f0ac652a678b302ac66bd23f0356d8d88
teukumufti <teukumufti9770@gmail.com>
2022-12-11 10:10:00 +0700

    fix button prev and next

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -24,12 +24,12 @@ function Services() {
 
             <div className="flex justify-between relative bottom-[260px]">
                 <button class="rounded-full text-slate-50 bg-aqua-100 ml-3 w-8 h-8" type="button">
-                    <svg className="inline m-auto" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 512" height="1.5em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="inline pr-1" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 512" height="1.5em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path>
                     </svg>
                 </button>
                 <button class="rounded-full text-slate-50 bg-aqua-100 mr-3 w-8 h-8" type="button">
-                    <svg className="inline m-auto" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 512" height="1.5em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="inline pl-1" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 512" height="1.5em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path>
                     </svg>
                 </button>
