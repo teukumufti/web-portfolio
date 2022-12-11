ae8fdd8f7b0fe0fde9016df07ac7d4b26b1b35aa
teukumufti <teukumufti9770@gmail.com>
2022-12-11 08:51:51 +0700

    update services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {data} from '../dataDummy/fakeData'
- 
 
 function Services() {
     return (
@@ -14,7 +13,7 @@ function Services() {
             <div className="grid grid-cols-3 justify-items-center mt-24">
                 {data.map(() =><div className="flex m-12">
                     <div className="border-l-2 text-aqua-100 h-36 mt-2">
-                        <img src={require('../assets/icons/git.png')} className="pt-7" />
+                        <img src={require('../assets/icons/git.png')} className="pt-7" alt="icons" />
                     </div>
                     <div className="pt-7 ml-5">
                         <span className="text-2xl text-slate-50 font-bold">GIT</span>
@@ -23,9 +22,17 @@ function Services() {
                 </div>)}
             </div>
 
-            <div className="flex justify-between">
-                <button class="rounded-full text-slate-50 bg-aqua-100" type="button">Back</button>
-                <button class="rounded-full text-slate-50 bg-aqua-100" type="button">Next</button>
+            <div className="flex justify-between relative bottom-[260px]">
+                <button class="rounded-full text-slate-50 bg-aqua-100 ml-3 w-8 h-8" type="button">
+                    <svg className="inline m-auto" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 512" height="1.5em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path>
+                    </svg>
+                </button>
+                <button class="rounded-full text-slate-50 bg-aqua-100 mr-3 w-8 h-8" type="button">
+                    <svg className="inline m-auto" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 512" height="1.5em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path>
+                    </svg>
+                </button>
             </div>
 
         </div>
