5a9fdf8b3f2c5fd99ef9b7cc6da2d1405769f8dd
teukumufti <teukumufti9770@gmail.com>
2022-12-11 08:53:23 +0700

    experience page

---
+++ src/pages/Experience.js
@@ -0,0 +1,15 @@
+import React from "react";
+ 
+
+function Experience() {
+    return (
+      <div>
+            <div>
+              <h1 className="text-3xl text-slate-50 text-center font-bold">MY EXPERIENCE</h1>
+              <div className="border-b-2 text-aqua-100 w-40 m-auto mt-2"></div>
+            </div>
+      </div>
+    )
+}
+
+export default Experience;
