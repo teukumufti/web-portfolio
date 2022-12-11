7bca235f0c8e280e60329a782f34cdf7ad2345ce
teukumufti <teukumufti9770@gmail.com>
2022-12-11 11:27:14 +0700

    fix page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,13 +1,43 @@
 import React from "react";
  
-
 function Experience() {
     return (
       <div>
-            <div>
-              <h1 className="text-3xl text-slate-50 text-center font-bold">MY EXPERIENCE</h1>
-              <div className="border-b-2 text-aqua-100 w-40 m-auto mt-2"></div>
-            </div>
+        <h1 className="text-3xl text-slate-50 text-center font-bold">MY EXPERIENCE</h1>
+        <div className="border-b-2 text-aqua-100 w-40 m-auto mt-2"></div>
+
+         
+         <div className="flex justify-center mt-32">
+
+          <div className="block p-6 rounded-sm bg-zinc-200 max-w-sm m-1">
+            <p className="text-gray-100"> March 2022 - Juni 2022</p>
+            <h5 className="text-aqua-100 text-xl font-medium">Web Engineer</h5>
+            <p className="text-gray-100">PT Merah Cipta Media</p>
+            <p className="text-gray-700 text-base mt-8 mb-10- text-white-50">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+            </p>
+          </div>
+
+          <div className="block p-6 rounded-sm bg-aqua-100 max-w-sm m-1">
+            <p className="text-zinc-100"> March 2022 - Juni 2022</p>
+            <h5 className="text-white-50 text-xl font-medium">Web Engineer</h5>
+            <p className="text-zinc-100">PT Merah Cipta Media</p>
+            <p className="text-gray-700 text-base mt-8 mb-10 text-white-50">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+            </p>
+          </div>
+
+          <div className="block p-6 rounded-sm bg-zinc-200 max-w-sm m-1">
+            <p className="text-gray-100"> March 2022 - Juni 2022</p>
+            <h5 className="text-aqua-100 text-xl font-medium">Web Engineer</h5>
+            <p className="text-gray-100">PT Merah Cipta Media</p>
+            <p className="text-gray-700 text-base mt-8 mb-10- text-white-50">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+            </p>
+          </div>
+          
+        </div>
+
       </div>
     )
 }
