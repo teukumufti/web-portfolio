ee3cd178514ebf2f5c75846027fd464c5233c2ff
teukumufti <teukumufti9770@gmail.com>
2022-12-16 11:24:17 +0700

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer()  {
+    return (
+        <div>
+            <footer className="text-center lg:text-left">
+            <div className="text-zinc-100 bg-aqua-100 text-center p-2 text-sm">
+            © 2022 Copyright:
+            <a className="text-gray-800" href="https://www.linkedin.com/in/teukumufti/" target='blank'>Teuku Mufti</a>
+            </div>
+            </footer>
+        </div>
+    )
+}
+
+ export default Footer;
