b7ae31c0ac541115884c3e26c36aafbb59f27589
teukumufti <teukumufti9770@gmail.com>
2022-12-16 11:23:15 +0700

    fix page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -30,14 +30,6 @@ function Contact() {
             SEND MESSAGE
             </button>
         </form>
-
-
-        <footer className="text-center lg:text-left">
-        <div className="text-zinc-100 bg-aqua-100 text-center p-2 text-sm">
-          © 2022 Copyright:
-          <a className="text-gray-800" href="https://www.linkedin.com/in/teukumufti/" target='blank'>Teuku Mufti</a>
-        </div>
-        </footer>
       </div>
     )
 }
