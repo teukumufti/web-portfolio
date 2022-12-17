7794d1f14a37163c1ec6ee42dbc8219e07ea9e2c
teukumufti <teukumufti9770@gmail.com>
2022-12-17 15:32:04 +0700

    fix

--- src/dataDummy/fakeData.js
+++ src/dataDummy/fakeData.js
@@ -1,21 +1,39 @@
+import icon1 from '../assets/icons/git.png'
+import icon2 from '../assets/icons/react.png'
+import icon3 from '../assets/icons/Node.png'
+import icon4 from '../assets/icons/mysql.png'
+import icon5 from '../assets/icons/sequalize.png'
+import icon6 from '../assets/icons/nuxt.png'
+
 export const data = [
     {
         id: 1,
-        title: 'SapientNitro',
+        title: 'GIT',
+        img: icon1,
     },
     {
         id: 2,
+        title: 'React JS',
+        img: icon2,
     },
     {
         id: 3,
+        title: 'Node JS',
+        img: icon3,
     },
     {
         id: 4,
+        title: 'MySQL',
+        img: icon4,
     },
     {
         id: 5,
+        title: 'Sequelize',
+        img: icon5,
     },
     {
         id: 6,
+        title: 'Nuxt JS',
+        img: icon6,
     },
 ]
